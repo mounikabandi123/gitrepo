08962c19cba99d5d294f21c42a1f035e84cdcdd8
mounikabandi123 <mounikabandi50@gmail.com>
2022-06-04 00:55:42 +0000

    deo code

---
+++ demo.java
@@ -0,0 +1,12 @@
+clsaa( )
+{
+	public static void main
+          {
+        s.o.print ("good day")
+        s.o.print ("good day") 	
+        s.o.print ("good day")	
+	s.o.print ("good day")
+	  }}
+
+
+
